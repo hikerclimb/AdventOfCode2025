658c926275211d65a21c0be54c8f79f61e6f486f
Aniket Karmarkar <karmarkaraniket@gmail.com>
2025-12-22 13:35:15 -0600

    problem 1 solution. I was overcomplicating it.

--- Day1Problem1.py
+++ Day1Problem1.py
@@ -2,39 +2,22 @@ def main():
     with open("Day1Problem1SampleInput.txt") as f:
         lines = f.readlines()
     counter = 0
-    start = 50
-    swtichSign = False
+    dialPosition = 50
+    #maxDialPosition = 100
+    numberOfRotations = 0
     for line in lines:
         direction = line[0]
-        print('line:' + line)
-        if direction == 'L' and (start - int(line[1:])%99) < 0:
-            print('enter left')
-            start = ((start - int(line[1:])+1) %99)
-        elif direction == 'R' and (start + int(line[1:])%99) > 0:
-            print('enter right')
-            start = ((start + int(line[1:])) % 100)
-        elif direction == 'L' and (start - int(line[1:])%99) > 0:
-            print("positiveleft")
-            start = ((start - int(line[1:])) % 99)
-        elif direction == 'R' and (start - int(line[1:])%99) < 0:
-            print("positiveright")
-            start = ((start - int(line[1:])) % 99)
-        if direction == 'L' and start == 0:
-            print('enter left sub 0')
-            start = ((start - int(line[1:])) %99)
-        elif direction == 'R' and start == 99:
-            print('enter right sub 0')
-            start = 99
-        elif direction == 'L' and (start - int(line[1:])%99) > 0:
-            print("positiveleft")
-            start = ((start - int(line[1:])) % 99)
-        elif direction == 'R' and (start - int(line[1:])%99) < 0:
-            print("positiveright")
-            start = ((start - int(line[1:])) % 99)
-        if start == 0:
-            
+        #print('line:' + line)
+        distance = (int)(line[1:])
+        #numberOfRotations = ((int)(distance / maxDialPosition)) + numberOfRotations
+        #print('dial position:' + str(dialPosition))
+        if direction == 'L':
+            dialPosition = (dialPosition - distance) % maxDialPosition
+        else:
+            dialPosition = (dialPosition + distance) % maxDialPosition
+        if dialPosition == 0:
             counter = counter +1
-        print('start:' + str(start))
-    print(counter)
+    print('counter:' + str(counter))
+    #print(counter+numberOfRotations)
 
 main()
