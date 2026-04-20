fd41e3fe84aeeef1a3854a5748798f565c5398bc
Aniket Karmarkar <karmarkaraniket@gmail.com>
2026-04-20 14:59:43 -0500

    added solution for problem 3 day 1.

---
+++ Day3Problem1.py
@@ -0,0 +1,23 @@
+def main():
+    with open("Day3Problem1Input.txt") as f:
+        summ = 0
+        for line in f:
+            numbers = []
+            startPosition = 0
+            numbersShortened = []
+            for index in range(0, len(line) -1):
+                if index == len(line) - 2:
+                    continue
+                else:
+                    numbers.append(line[index])
+            if numbers == []:
+                continue
+            else:
+                startPosition = numbers.index(str(max(numbers))) + 1
+            for index in range(startPosition, len(line)):
+                numbersShortened.append(line[index])
+            numJoint = int(max(numbers)+ max(numbersShortened))
+            summ += numJoint
+        print(summ)
+            
+main()
