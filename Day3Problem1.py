43c459ce3a9d873ddcf6a54e4660845fa48c8a82
Aniket Karmarkar <karmarkaraniket@gmail.com>
2026-04-20 20:21:35 -0500

    removed unnecessary code from Day3Problem1.py

--- Day3Problem1.py
+++ Day3Problem1.py
@@ -7,13 +7,10 @@ def main():
             numbersShortened = []
             for index in range(0, len(line) -1):
                 if index == len(line) - 2:
-                    continue
+                   continue
                 else:
                     numbers.append(line[index])
-            if numbers == []:
-                continue
-            else:
-                startPosition = numbers.index(str(max(numbers))) + 1
+            startPosition = numbers.index(str(max(numbers))) + 1
             for index in range(startPosition, len(line)):
                 numbersShortened.append(line[index])
             numJoint = int(max(numbers)+ max(numbersShortened))
